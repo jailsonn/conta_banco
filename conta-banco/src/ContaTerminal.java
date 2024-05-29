dcf019e403074b0d9558a090ab07efaa9db65d7a
jailsonn <jailson.nogueira2@gmail.com>
2024-05-29 13:39:30 -0300

    continuando o projeto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,20 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TODO: Conhecer e importar a classe Scanner
+
+        int numeroConta = 1021;
+        String agencia = "067-8";
+        String nomeCliente = "MARIO ANDRADE";
+        Double saldo = 237.48;
+
+        
+
+        // Exibir as mensagens para o nosso usuário
+
+        // Obter pela scanner os valores digitados no terminal
+
+        // Exibir a mensagem conta criada
+
+
     }
 }
